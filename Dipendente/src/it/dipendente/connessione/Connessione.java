c74ba1e085116000fd3bf23a4ebfa468c07c7a1e
gabriele.bertaina <gabriele.bertaina@gmail.com>
2012-11-10 14:51:29 +0000

    close e connessione

--- Dipendente/src/it/dipendente/connessione/Connessione.java
+++ Dipendente/src/it/dipendente/connessione/Connessione.java
@@ -1,31 +1,60 @@
 package it.dipendente.connessione;
 
+import it.dipendente.util.MyLogger;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connessione {
-	
-	public Connection connessione(String dataBase){		
-		Connection con = null;		
+	private MyLogger log;
+	private Connection connection;
+
+	public Connessione() {
+		log =new MyLogger(this.getClass());
+		final String metodo="costruttore";
+		log.start(metodo);
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
-			System.out.println("driver non caricati");
-			e.printStackTrace();
-		}		
+			log.fatal(metodo, "driver non caricati", e);
+		}
 		try {
-			if(dataBase.equals("cvonline")){
-				con = DriverManager.getConnection("jdbc:mysql://151.1.159.238:3306/cvonline_Curriculum","cvonline_DiErre","DiErre2012");
-//				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cvonline_Curriculum","root","root");
-			}else if(dataBase.equals("drconsulting")){
-				con = DriverManager.getConnection("jdbc:mysql://151.1.159.238:3306/drcon860_curriculum","drcon860_DiErre","DiErre2012");
-//				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/drcon860_curriculum","root","root");
-			}
+			connection = DriverManager.getConnection("jdbc:mysql://151.1.159.238:3306/drcon860_curriculum","drcon860_DiErre","DiErre2012");
+//			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/drcon860_curriculum","root","root");
 		} catch (SQLException e) {
-			System.out.println("connessione fallita");
-			e.printStackTrace();
-		}		
-		return con;
+			log.fatal(metodo, "connessione fallita", e);
+		}
+	}
+
+	/**
+	 * distruttore
+	 */
+	protected void finalize(){
+		closeConnection();
+	}
+
+	/**
+	 *  close the Connection
+	 */
+	@Deprecated
+	public void closeConnection(){
+		final String metodo="closeConnection";
+		log.start(metodo);
+		if (connection!=null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				log.warn(metodo, "tentativo chiusura connessione db", e);
+			}
+		}
+		log.end(metodo);
+	}
+
+	/**
+	 * @return the connection
+	 */
+	public Connection getConnection() {
+		return connection;
 	}
 }
