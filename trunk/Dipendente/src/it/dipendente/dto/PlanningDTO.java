65674af50d23fb0c63ee4849eed0f77b0dca81d7
gabriele.bertaina@gmail.com <gabriele.bertaina@gmail.com@a6818843-402c-da00-0640-74d31253e89f>
2012-11-13 19:42:31 +0000

    new time report

--- trunk/Dipendente/src/it/dipendente/dto/PlanningDTO.java
+++ trunk/Dipendente/src/it/dipendente/dto/PlanningDTO.java
@@ -16,6 +16,8 @@ public class PlanningDTO {
 	private boolean attivo;
 	private boolean commessaAttiva;
 
+	private boolean hasOvertime;
+
 	public PlanningDTO() {}
 	public PlanningDTO(int id_planning,
 						Date data,
@@ -36,84 +38,138 @@ public class PlanningDTO {
 		this.id_associazione=id_associazione;
 		this.descr_attivita=descr_attivita;
 	}
+
+	/**
+	 * @return the id_planning
+	 */
 	public int getId_planning() {
 		return id_planning;
 	}
-
+	/**
+	 * @param id_planning the id_planning to set
+	 */
 	public void setId_planning(int id_planning) {
 		this.id_planning = id_planning;
 	}
-
+	/**
+	 * @return the data
+	 */
 	public Calendar getData() {
 		return data;
 	}
-
+	/**
+	 * @param data the data to set
+	 */
 	public void setData(Calendar data) {
 		this.data = data;
 	}
-
+	/**
+	 * @return the numeroOre
+	 */
 	public double getNumeroOre() {
 		return numeroOre;
 	}
-
+	/**
+	 * @param numeroOre the numeroOre to set
+	 */
 	public void setNumeroOre(double numeroOre) {
 		this.numeroOre = numeroOre;
 	}
-
+	/**
+	 * @return the straordinari
+	 */
 	public double getStraordinari() {
 		return straordinari;
 	}
-
+	/**
+	 * @param straordinari the straordinari to set
+	 */
 	public void setStraordinari(double straordinari) {
 		this.straordinari = straordinari;
 	}
-
+	/**
+	 * @return the descr_attivita
+	 */
 	public String getDescr_attivita() {
 		return descr_attivita;
 	}
-
+	/**
+	 * @param descr_attivita the descr_attivita to set
+	 */
 	public void setDescr_attivita(String descr_attivita) {
 		this.descr_attivita = descr_attivita;
 	}
-
+	/**
+	 * @return the orario
+	 */
 	public String getOrario() {
 		return orario;
 	}
-
+	/**
+	 * @param orario the orario to set
+	 */
 	public void setOrario(String orario) {
 		this.orario = orario;
 	}
-
+	/**
+	 * @return the id_associazione
+	 */
 	public int getId_associazione() {
 		return id_associazione;
 	}
-
+	/**
+	 * @param id_associazione the id_associazione to set
+	 */
 	public void setId_associazione(int id_associazione) {
 		this.id_associazione = id_associazione;
 	}
-
+	/**
+	 * @return the note
+	 */
 	public String getNote() {
 		return note;
 	}
-
+	/**
+	 * @param note the note to set
+	 */
 	public void setNote(String note) {
 		this.note = note;
 	}
-
+	/**
+	 * @return the attivo
+	 */
 	public boolean isAttivo() {
 		return attivo;
 	}
-
+	/**
+	 * @param attivo the attivo to set
+	 */
 	public void setAttivo(boolean attivo) {
 		this.attivo = attivo;
 	}
-
+	/**
+	 * @return the commessaAttiva
+	 */
 	public boolean isCommessaAttiva() {
 		return commessaAttiva;
 	}
-
+	/**
+	 * @param commessaAttiva the commessaAttiva to set
+	 */
 	public void setCommessaAttiva(boolean commessaAttiva) {
 		this.commessaAttiva = commessaAttiva;
+	}
+	/**
+	 * @return the hasOvertime
+	 */
+	public boolean isHasOvertime() {
+		return hasOvertime;
+	}
+	/**
+	 * @param hasOvertime the hasOvertime to set
+	 */
+	public void setHasOvertime(boolean hasOvertime) {
+		this.hasOvertime = hasOvertime;
 	}
 
 	public String toString(){
