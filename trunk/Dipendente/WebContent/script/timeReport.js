bdad7a1c3d0d5c48d8f747fe422bb276a05731f6
emmanuel.devita@gmail.com <emmanuel.devita@gmail.com@a6818843-402c-da00-0640-74d31253e89f>
2012-11-23 11:24:15 +0000

    aggiunto controllo sul formato delle ore. Consolidato su tutti i browser.

--- trunk/Dipendente/WebContent/script/timeReport.js
+++ trunk/Dipendente/WebContent/script/timeReport.js
@@ -13,13 +13,20 @@ function validate(evt) {
 function checkOrario(input) {
 	if (input.value.length>3) {
 		alert("Attenzione! Il numero di ore inserito:"+input.value+" non è valido.");
-		input.value="0,0";
+		input.value="0.0";
 		input.focus();
 		return false;
+	}else{
+		if(input.value.indexOf(".") == -1){
+			alert("Attenzione! Il formato del numero ore inserito non è valido. Formato corretto 0.0");
+			input.value="0.0";
+			input.focus();
+			return false;
+		}
 	}
 	if(input.value>24){
 		alert("Attenzione! Superato il numero massimo di ore per commessa.");
-		input.value="0,0";
+		input.value="0.0";
 		input.focus();
 		return false;
 	}
