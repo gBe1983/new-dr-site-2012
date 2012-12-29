e9732fe8e2af2f7ff2c795dbd573d7296c74b460
gabriele.bertaina@gmail.com <gabriele.bertaina@gmail.com@a6818843-402c-da00-0640-74d31253e89f>
2012-12-29 00:56:20 +0000

    implementazioni su time report

--- trunk/Dipendente/src/it/dipendente/bo/Month.java
+++ trunk/Dipendente/src/it/dipendente/bo/Month.java
@@ -3,15 +3,13 @@ package it.dipendente.bo;
 import it.dipendente.dto.PlanningDTO;
 import it.util.log.MyLogger;
 
-import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
-public class Month implements Serializable{
-	private static final long serialVersionUID = -3196180487587957038L;
+public class Month{
 	private MyLogger log;
 	private int year;
 	private int month;
@@ -100,7 +98,117 @@ public class Month implements Serializable{
 	public boolean isSavable(){
 		//TODO IMPLEMENTARE LA MODIFICA CHE VERIFICA SE IL MESE RISULTA CHIUSO
 		Calendar today=Calendar.getInstance();
-		return (year>today.get(Calendar.YEAR)||
-					(year==today.get(Calendar.YEAR)&&month>=today.get(Calendar.MONTH)));
+		int y = today.get(Calendar.YEAR);
+		if (year>y){
+			return true;
+		}
+		int m = today.get(Calendar.MONTH);
+		if(year==y){
+			if(month>=m){
+				return true;
+			}
+			if(m-month<=2){
+				return true;
+			}
+			return false;
+		}
+		if(year<=y && y-year==1){
+			if(month>m && month-m<=2){
+				return true;
+			}
+			if(month<m && m-month<=2){
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
+
+	public String getBodyMail(){
+		double totOreOrd=0;
+		double totOreStr=0;
+		SimpleDateFormat sdf=new SimpleDateFormat("d");
+		StringBuilder sb = new StringBuilder("<table align='center' border='1'>");
+		String dayInLineStyle;
+		for(Week w:weeks){
+			sb	.append("<tr><th colspan='8' style='font:bold 12px verdana, arial, sans-serif;color:#A1A1A1;background:#CCC;'>Settimana")
+				.append(w.getWeekOfYear())
+				.append("</th></tr><tr><th style='font:normal 12px verdana, arial, sans-serif;color:#A1A1A1;'>Commesse Abilitate</th>");
+			for(Day d:w.getDays()){
+				dayInLineStyle=d.getInLineStyle();
+				sb	.append("<th style='")
+					.append(dayInLineStyle)
+					.append("'><table align='center'><tr><td align='center' style='")
+					.append(dayInLineStyle)
+					.append("'>")
+					.append(d.getDayLabel())
+					.append("</td></tr><tr><td align='center' style='")
+					.append(dayInLineStyle)
+					.append("'>");
+				if(d.getDay()!=null){
+					sb.append(sdf.format(d.getDay().getTime()));
+				}else{
+					sb.append("&nbsp;");
+				}
+				sb.append("</td></tr></table></th>");
+			}
+			sb.append("</tr>");
+			if(w.getCommesse().isEmpty()){
+				sb.append("<tr><th colspan='8' style='font:bold 12px verdana,arial,sans-serif;color:red;background:#CCC;'>ATTENZIONE, PER TALE SETTIMANA NON RISULTA ALCUNA COMMESSA ASSOCIATA</th></tr>");
+			}else{
+				for(String commessaKey:w.getCommesse().keySet()){
+					sb	.append("<tr><th style='font:normal 12px verdana, arial, sans-serif;color:blue;background:#E7D6B6;'>")
+						.append(commessaKey)
+						.append("</th>");
+					for(Day d:w.getDays()){
+						dayInLineStyle=d.getInLineStyle();
+						sb	.append("<td style='")
+							.append(dayInLineStyle)
+							.append("'>");
+						if(d.getDay()!=null){
+							for(PlanningDTO p:(List<PlanningDTO>)w.getCommesse().get(commessaKey)){
+								if(p.getData().get(Calendar.DAY_OF_MONTH)==d.getDay().get(Calendar.DAY_OF_MONTH)){
+									totOreOrd+=p.getNumeroOre();
+									totOreStr+=p.getStraordinari();
+									sb	.append("<table align='center'><tr><td align='center' style='")
+										.append(dayInLineStyle)
+										.append("'>")
+										.append(p.getNumeroOre())
+										.append("</td></tr><tr><td align='center' style='")
+										.append(dayInLineStyle)
+										.append("'>")
+										.append(p.getStraordinari())
+										.append("</td></tr></table>");
+									break;
+								}
+							}
+						}
+						sb.append("</td>");
+					}
+					sb.append("</tr>");
+				}
+			}
+		}
+		String riepilogoInLineStyle="font:bold 12px verdana, arial, sans-serif;color:red;background:#DDD;";
+		sb	.append("<tr><td style='")
+			.append(riepilogoInLineStyle)
+			.append("'>Ore ordinarie:</td><td style='")
+			.append(riepilogoInLineStyle)
+			.append("'>")
+			.append(totOreOrd)
+			.append("</td><td style='")
+			.append(riepilogoInLineStyle)
+			.append("'>Ore straordinarie:</td><td style='")
+			.append(riepilogoInLineStyle)
+			.append("'>")
+			.append(totOreStr)
+			.append("</td><td style='")
+			.append(riepilogoInLineStyle)
+			.append("'>Totale:</td><td colspan='3' style='")
+			.append(riepilogoInLineStyle)
+			.append("'>")
+			.append(totOreOrd+totOreStr)
+			.append("</td></tr></table>");
+		return sb.toString();
 	}
 }
