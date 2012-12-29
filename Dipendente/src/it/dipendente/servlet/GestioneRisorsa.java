8789fb5fa5f3b0348ebd7e925828a9efed13220f
gabriele.bertaina <gabriele.bertaina@gmail.com>
2012-12-29 12:11:30 +0000

    Centralizzazzione pulizzia sessione Centralizzata gestione mail Aggiunti parametri mail e urlSite al web.xml

--- Dipendente/src/it/dipendente/servlet/GestioneRisorsa.java
+++ Dipendente/src/it/dipendente/servlet/GestioneRisorsa.java
@@ -5,9 +5,7 @@ import it.dipendente.dto.RisorsaDTO;
 import it.util.log.MyLogger;
 
 import java.io.IOException;
-import java.util.Enumeration;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class GestioneRisorsa extends BaseServlet {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -9074701279617210593L;
 	private MyLogger log;
 
 	public GestioneRisorsa() {
@@ -48,101 +46,69 @@ public class GestioneRisorsa extends BaseServlet {
 		log.start(metodo);
 		HttpSession sessione = request.getSession();
 		RisorsaDAO rDAO = new RisorsaDAO(conn.getConnection());
-		RequestDispatcher rd = null;
-		//recupero il parametro azione
 		String azione = request.getParameter("azione");
-		
 		if(azione.equals("login")){
-			
 			int idRisorsa = Integer.parseInt(request.getParameter("parametro"));
-			
 			log.debug(metodo, "login idRisorsa" + idRisorsa);
-			
-			RisorsaDTO risorsa = rDAO.loginRisorsa(idRisorsa);
-			sessione.setAttribute("utenteLoggato",risorsa);
+			sessione.setAttribute("utenteLoggato",rDAO.loginRisorsa(idRisorsa));
 			response.sendRedirect("./index.jsp?azione=benvenuto");
-			
 		}
 		if(sessione.getAttribute("utenteLoggato") != null){
 			if(azione.equals("visualizzaProfilo") || azione.equals("aggiornaProfilo")){
-				
-				
 				RisorsaDTO risorsa = rDAO.visualizzaProfilo(((RisorsaDTO)sessione.getAttribute("utenteLoggato")).getIdRisorsa());
 				request.setAttribute("risorsa", risorsa);
-				if(azione.equals("visualizzaProfilo")){
-					rd = getServletContext().getRequestDispatcher("/index.jsp?azione=visualizzaProfilo&dispositiva=areaPrivata");
-				}else{
-					rd = getServletContext().getRequestDispatcher("/index.jsp?azione=aggiornaProfilo&dispositiva=areaPrivata");
-				}
-				rd.forward(request, response);
-				
+				getServletContext()
+					.getRequestDispatcher(azione.equals("visualizzaProfilo")?
+											"/index.jsp?azione=visualizzaProfilo&dispositiva=areaPrivata":
+											"/index.jsp?azione=aggiornaProfilo&dispositiva=areaPrivata")
+						.forward(request, response);
 			}else if(azione.equals("modificaRisorsa")){
-				
-				RisorsaDTO risorsa = new RisorsaDTO(((RisorsaDTO)sessione.getAttribute("utenteLoggato")).getIdRisorsa(),
-																		request.getParameter("cognome"),
-																		request.getParameter("nome"),
-																		request.getParameter("dataNascita"),
-																		request.getParameter("luogoNascita"),
-																		request.getParameter("sesso"),
-																		request.getParameter("codiceFiscale"),
-																		request.getParameter("mail"),
-																		request.getParameter("telefono"),
-																		request.getParameter("cellulare"),
-																		request.getParameter("fax"),
-																		request.getParameter("indirizzo"),
-																		request.getParameter("citta"),
-																		request.getParameter("provincia"),
-																		request.getParameter("cap"),
-																		request.getParameter("nazione"),
-																		request.getParameter("militare"),
-																		request.getParameter("patente"),
-																		request.getParameter("costo"),
-																		request.getParameter("occupato").equals("si"),
-																		request.getParameter("tipo_contratto"),
-																		request.getParameter("figuraProfessionale"),
-																		request.getParameter("seniority"),
-																		true,
-																		true,
-																		true);
-				
+				RisorsaDTO risorsa =
+					new RisorsaDTO(	((RisorsaDTO)sessione.getAttribute("utenteLoggato")).getIdRisorsa(),
+									request.getParameter("cognome"),
+									request.getParameter("nome"),
+									request.getParameter("dataNascita"),
+									request.getParameter("luogoNascita"),
+									request.getParameter("sesso"),
+									request.getParameter("codiceFiscale"),
+									request.getParameter("mail"),
+									request.getParameter("telefono"),
+									request.getParameter("cellulare"),
+									request.getParameter("fax"),
+									request.getParameter("indirizzo"),
+									request.getParameter("citta"),
+									request.getParameter("provincia"),
+									request.getParameter("cap"),
+									request.getParameter("nazione"),
+									request.getParameter("militare"),
+									request.getParameter("patente"),
+									request.getParameter("costo"),
+									request.getParameter("occupato").equals("si"),
+									request.getParameter("tipo_contratto"),
+									request.getParameter("figuraProfessionale"),
+									request.getParameter("seniority"),
+									true,
+									true,
+									true);
 				String messaggio = rDAO.modificaRisorsa(risorsa);
-				if(messaggio.equals("ok")){
-					request.setAttribute("messaggio", "La modifica del profilo è avvenuta correttamente");
-					rd = getServletContext().getRequestDispatcher("/index.jsp?azione=messaggio&dispositiva=areaPrivata");
-					rd.forward(request, response);
-				}else{
-					request.setAttribute("messaggio", messaggio);
-					rd = getServletContext().getRequestDispatcher("/index.jsp?azione=messaggio&dispositiva=areaPrivata");
-					rd.forward(request, response);
-				}
-				
+				request.setAttribute("messaggio", messaggio.equals("ok")?
+													"La modifica del profilo è avvenuta correttamente":
+													messaggio);
+				getServletContext()
+					.getRequestDispatcher("/index.jsp?azione=messaggio&dispositiva=areaPrivata")
+						.forward(request, response);
 			}else if(azione.equals("modificaPassword")){
-				
-				String messaggio = rDAO.modificaPassword(((RisorsaDTO)sessione.getAttribute("utenteLoggato")).getIdRisorsa(),request.getParameter("nuovaPassword"));
-				if(messaggio.equals("ok")){
-					request.setAttribute("messaggio", "La modifica della password è avvenuta correttamente");
-					rd = getServletContext().getRequestDispatcher("/index.jsp?azione=messaggio&dispositiva=areaPrivata");
-					rd.forward(request, response);
-				}else{
-					request.setAttribute("messaggio", messaggio);
-					rd = getServletContext().getRequestDispatcher("/index.jsp?azione=messaggio&dispositiva=areaPrivata");
-					rd.forward(request, response);
-				}
-				
+				String messaggio = rDAO.modificaPassword(	((RisorsaDTO)sessione.getAttribute("utenteLoggato")).getIdRisorsa(),
+															request.getParameter("nuovaPassword"));
+				request.setAttribute("messaggio", messaggio.equals("ok")?
+													"La modifica della password è avvenuta correttamente":
+													messaggio);
+				getServletContext()
+					.getRequestDispatcher("/index.jsp?azione=messaggio&dispositiva=areaPrivata")
+						.forward(request, response);
 			}else if(azione.equals("logout")){
-				
-				Enumeration attrNames = sessione.getAttributeNames();
-				while (attrNames.hasMoreElements())	{		  
-					String valoriSessione = (String) attrNames.nextElement();// Stampa i nomi di tutti gli attributi di sessione
-					if(valoriSessione.equals("modalitaDiConnessione")){
-						continue;
-					}else{
-						sessione.removeAttribute(valoriSessione);
-						log.debug(metodo, "logout " + valoriSessione);
-					}
-				}
-				
-				response.sendRedirect("http://drconsulting.tv");
+				clearSession(sessione);
+				response.sendRedirect(prop.getProperty("siteUrl"));
 			}
 		}else{
 			sessioneScaduta(response);
